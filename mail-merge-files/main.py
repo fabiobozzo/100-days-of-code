aad5af7f5721eef8d357046e7a1ae63495bbc7dd
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-08 12:40:42 +0100

    mail merge file solution

--- mail-merge-files/main.py
+++ mail-merge-files/main.py
@@ -4,7 +4,7 @@ with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
 with open("./Input/Names/invited_names.txt") as invited_names_file:
     invited_names = invited_names_file.readlines()
     for name in invited_names:
-        new_letter = starting_letter.replace("[name]", name.strip().replace("\n", ""))
+        new_letter = starting_letter.replace("[name]", name.strip())
         new_letter_file = open(f"./Output/ReadyToSend/letter_{name.lower()}.txt", mode="w")
         new_letter_file.write(new_letter)
         new_letter_file.close()

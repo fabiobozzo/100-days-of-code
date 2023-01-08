505b997bc2b178d1dcf78b8c4460416846f3d204
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-08 12:34:19 +0100

    mail merge file solution

--- mail-merge-files/main.py
+++ mail-merge-files/main.py
@@ -1,8 +1,10 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
+    starting_letter = starting_letter_file.read()
+
+with open("./Input/Names/invited_names.txt") as invited_names_file:
+    invited_names = invited_names_file.readlines()
+    for name in invited_names:
+        new_letter = starting_letter.replace("[name]", name.strip().replace("\n", ""))
+        new_letter_file = open(f"./Output/ReadyToSend/letter_{name.lower()}.txt", mode="w")
+        new_letter_file.write(new_letter)
+        new_letter_file.close()

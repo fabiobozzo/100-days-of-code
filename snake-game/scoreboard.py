bd393dbfacf50f8898c429722148ace592c920cb
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-08 12:02:41 +0100

    save snake high score to file

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -8,8 +8,13 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+
         self.score = 0
         self.high_score = 0
+        with open("data.txt") as file:
+            file_content = file.read()
+            self.high_score = int(file_content.replace("\n", "").strip())
+
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -32,5 +37,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+
         self.score = 0
         self.update_scoreboard()

61534a43abdb781abfe0225d3dc99427983c18ff
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-08 11:44:54 +0100

    files in python primer

---
+++ scratches/day-24/main.py
@@ -0,0 +1,18 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# mode="r" -> read-only
+# mode="w" -> write
+# mode="a" -> append
+with open("new_file.txt", mode="w") as file:
+    file.write("Hello, my name is Sebastiano.")
+
+with open("new_file.txt", mode="a") as file:
+    file.write("\nCiao.")
+
+with open("new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+

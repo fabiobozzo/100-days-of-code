8409391a3e2ac7f8f0191a57699df5e0707e1b62
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-15 12:27:35 +0100

    bidirectional miles-to-km converter

--- scratches/day-27-tkinter/miles_km_converter.py
+++ scratches/day-27-tkinter/miles_km_converter.py
@@ -1,44 +1,60 @@
 import tkinter
 
 
-def calculate():
-    val = entry.get()
+def calculate_km():
+    error_label.config(text="")
+    val = miles_entry.get()
     if not val.isdigit():
-        result_label.config(text="Error")
-        entry.delete(0, tkinter.END)
-        entry.insert(0, "0")
+        error_label.config(text="Error")
+        miles_entry.delete(0, tkinter.END)
+        miles_entry.insert(0, "0")
     else:
-        result = round(int(val) * 1.6, 2)
-        result_label.config(text=str(result))
+        result = round(int(val) * 1.609)
+        km_entry.delete(0, tkinter.END)
+        km_entry.insert(0, str(result))
+
+
+def calculate_miles():
+    error_label.config(text="")
+    val = km_entry.get()
+    if not val.isdigit():
+        error_label.config(text="Error")
+        km_entry.delete(0, tkinter.END)
+        km_entry.insert(0, "0")
+    else:
+        result = round(int(val) / 1.609)
+        miles_entry.delete(0, tkinter.END)
+        miles_entry.insert(0, str(result))
 
 
 window = tkinter.Tk()
 
 window.title("Miles to Km Converter")
-window.minsize(width=400, height=250)
-window.config(padx=50, pady=50)
+window.config(padx=20, pady=20)
 
-entry = tkinter.Entry(width=10)
-entry.insert(0, string="0")
-entry.grid(row=0, column=1)
+to_miles_button = tkinter.Button(text="to Miles", command=calculate_miles)
+to_miles_button.grid(row=0, column=0)
+
+miles_entry = tkinter.Entry(width=10)
+miles_entry.insert(0, string="0")
+miles_entry.grid(row=0, column=1)
 
 miles_label = tkinter.Label(text="Miles")
 miles_label.grid(row=0, column=2)
 miles_label.config(padx=10, pady=10)
 
-equalto_label = tkinter.Label(text="is equal to")
-equalto_label.grid(row=1, column=0)
-equalto_label.config(padx=10, pady=10)
+to_km_button = tkinter.Button(text="to Km", command=calculate_km)
+to_km_button.grid(row=1, column=0)
 
-result_label = tkinter.Label(text="0", font=("Arial", 18, "bold"))
-result_label.grid(row=1, column=1)
-result_label.config(padx=10, pady=10)
+km_entry = tkinter.Entry(width=10)
+km_entry.grid(row=1, column=1)
+km_entry.insert(0, string="0")
 
-km_label = tkinter.Label(text="Km")
+km_label = tkinter.Label(text="Kilometers")
 km_label.grid(row=1, column=2)
 km_label.config(padx=10, pady=10)
 
-calculate_button = tkinter.Button(text="Calculate", command=calculate)
-calculate_button.grid(row=2, column=1)
+error_label = tkinter.Label(text="")
+error_label.grid(row=2, column=1)
 
 window.mainloop()

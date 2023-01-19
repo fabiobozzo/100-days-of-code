204af993dc06198e51b85b09fe2093b3ec15c4c2
Fabio Bozzo <fabio.bozzo@gmail.com>
2023-01-19 11:44:37 +0100

    nato alphabet exception handling

--- scratches/nato-alphabet/main.py
+++ scratches/nato-alphabet/main.py
@@ -9,6 +9,13 @@ codes_dictionary = {row.letter: row.code for (_, row) in df.iterrows()}
 # print(codes_dictionary)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("A word you want to get the phonetic codes of: ")
-user_word_codes = [codes_dictionary[letter.upper()] for letter in user_word]
-print(user_word_codes)
+done = False
+while not done:
+    try:
+        user_word = input("A word you want to get the phonetic codes of: ")
+        user_word_codes = [codes_dictionary[letter.upper()] for letter in user_word]
+        print(user_word_codes)
+    except KeyError as k:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        done = True
